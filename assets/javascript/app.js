d25498baaf9ddd7050fd9aab02c00bac760a625c
thomromano <thomromano@gmail.com>
2018-03-30 19:53:10 -0500

    add firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+"use strict"
+
+
+$(document).ready(function() {
+
+    const trainData = new Firebase("https://thomproject-e6ae1.firebaseio.com/");
+
+    $("#addButton").on("click", function() {
+        
+        let train = $("#trainName").val().trim();
+    })
+}
