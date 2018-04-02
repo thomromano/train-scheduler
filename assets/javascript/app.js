80c087f388ef74e5fdb20b2940cf536139e81595
thomromano <thomromano@gmail.com>
2018-04-02 15:48:37 -0500

    rename ids for consistency

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,66 @@
 "use strict"
 
+//Initialize firebase
+var config = {
+    apiKey: "AIzaSyBf-RRTMGdxJNWguY0KlSg09SFQm-IMyMY",
+    authDomain: "thomproject-e6ae1.firebaseapp.com",
+    databaseURL: "https://thomproject-e6ae1.firebaseio.com",
+    projectId: "thomproject-e6ae1",
+    storageBucket: "thomproject-e6ae1.appspot.com",
+    messagingSenderId: "631388206573"
+};
 
-$(document).ready(function() {
+firebase.initializeApp(config);
 
-    var config = {
-        apiKey: "AIzaSyBf-RRTMGdxJNWguY0KlSg09SFQm-IMyMY",
-        authDomain: "thomproject-e6ae1.firebaseapp.com",
-        databaseURL: "https://thomproject-e6ae1.firebaseio.com",
-        projectId: "thomproject-e6ae1",
-        storageBucket: "thomproject-e6ae1.appspot.com",
-        messagingSenderId: "631388206573"
-      };
-      firebase.initializeApp(config);
+const database = firebase.database();
 
-    $("#addButton").on("click", function() {
-        
-        let train = $("#trainName").val().trim();
-        let line = $("#line").val().trim();
-        let destination = $("#destination").val().trim();
-        let trainTime = moment($("#trainTime").val().trim(), "HH:mm");
-        let frequency = $("#frequency").val().trim();
 
-        console.log(train);
-        console.log(line);
-        console.log(destination);
-        console.log(trainTime);
-        console.log(frequency);
+//Button to add new trains
+$("#add-train-button").on("click", function (e) {
+    e.preventDefault();
 
-        let newTrain = {
-            name: train,
-            line: line,
-            destination: destination,
-            trainTime: trainTime,
-            frequency: frequency,
-        }
+    //grab user input
+    let train = $("#train-name-input").val().trim();
+    let line = $("#line-input").val().trim();
+    let destination = $("#destination-input").val().trim();
+    let trainTime = moment($("#time-input").val().trim(), "HH:mm");
+    let frequency = $("#frequency-input").val().trim();
 
-        config.push(newTrain);
-
-        $("#trainName").val("");
-        $("#line").val("");
-        $("#destination").val("");
-        $("#trainTime").val("");
-        $("#frequency").val("");
-
-        return false;
-
-    });
     
-}
+//create local object for holding train data
+    let newTrain = {
+        name: train,
+        line: line,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency,
+    };
+
+    //upload train data to database
+    database.ref().push(newTrain);
+
+    //log to console
+console.log(newTrain.train);
+    console.log(newTrain.line);
+    console.log(newTrain.destination);
+    console.log(newTrain.trainTime);
+    console.log(newTrain.frequency);
+
+
+//Clear textboxes
+    $("#trainName").val("");
+    $("#line").val("");
+    $("#destination").val("");
+    $("#trainTime").val("");
+    $("#frequency").val("");
+
+});
+
+//Creates firebase event for adding train to database and a row in html
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+    console.log(childSnapshot.val());
+
+    //Store everything into a variable 
+
+})
